7d914ec52978a9c077ac0ee231bb80e8b9038f27
Anshita Bhasin <anshita.bhasin@gmail.com>
2024-06-15 22:40:02 +0400

    Added Allure Report for both Cypress & Playwright

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
+
 
 module.exports = defineConfig({
 
   e2e: {
     specPattern: "cypress/e2e/**/*.js",
-    //"cypress/e2e/**/*.test.js" ,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on);
+
     },
   },
 });
